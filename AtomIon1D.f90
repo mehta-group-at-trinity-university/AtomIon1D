7558b0365d265da0e1edfcf9a8b4d8df5ae9403a
Nirav Mehta <nmehta@trinity.edu>
2026-03-04 14:53:17 -0600

    Changed a few things to incorporate an effective potential.  Commented out all the stuff having to do with nonadiabatic couplings and diabatization

--- AtomIon1D.f90
+++ AtomIon1D.f90
@@ -89,8 +89,8 @@ program AtomIon1D
   double precision, allocatable :: NewPsi(:,:),TempEnergies(:,:)
   double precision, allocatable :: P(:,:,:),QTil(:,:,:),dP(:,:), testmat(:,:),SPsi(:)
   double precision ur(1:50000),acoef,bcoef,diff
-  double precision sec,time,Rinitial,secp,timep,Rvalue, sNb, sbc, C4,lho
-  double precision hbar, phi, amu,omega,Rstar, dum,Dtol,testorth
+  double precision sec,time,Rinitial,secp,timep,Rvalue, sNb, sbc, C4,lho,atest,c0
+  double precision hbar, phi, amu,omega,Rstar, dum,Dtol,testorth,Estar
   double precision, external :: ddot, kdelta
   character*64 LegendreFile
   character*64 leftnrg
@@ -134,17 +134,18 @@ program AtomIon1D
   mgamma = mu
   theta_c = datan(mgamma)     ! same as Seth's beta !!! arctan mu, d?atan
   write(6,*) "theta_c = ", theta_c
-
-  ! read in grid information
+  
+  ! read in grid information, atest is the length scale of the short-range effective potential in units of Rstar
   read(5,*)
   read(5,*)
-  read(5,*) xNumPoints, omega, Nbs, C4, phi,Dtol
-  write(6,*) xNumPoints, omega, Nbs, C4, phi,Dtol
+  read(5,*) xNumPoints, omega, Nbs, C4, phi,Dtol, atest, c0
+  write(6,*) xNumPoints, omega, Nbs, C4, phi,Dtol, atest, c0
 
   omega = 2d0*Pi*omega
   lho = dsqrt(hbar/mi/omega)
   Rstar = dsqrt(2*mu12*C4/hbar**2)
-  C4 = C4/(hbar*omega*lho**4)
+  Estar = hbar**2/(2d0*mu12*Rstar**2)
+  C4 = C4/(hbar*omega*lho**4)  ! Convert C4 to oscillator units here.
   sNb = 1.d0/(dble(Nbs+1)*Pi + phi) !We use Nbs+1 here since the scattering length (and therefore phi=1/a) is negative. 
 
   write(6,*) "lho = ", lho
@@ -156,7 +157,9 @@ program AtomIon1D
   ma = ma/mi
   mi = 1d0
   mu12=mi*ma/(mi+ma)  ! ion-atom reduced mass (mu remains unchanged)
-
+  ! convert atest to oscillator units
+  atest = atest*Rstar/lho
+  c0 = c0*Estar/(hbar*omega)
   read(5,*)
   read(5,*)
   read(5,*) RSteps,RDerivDelt,RFirst,RLast, OPGRID
@@ -254,8 +257,8 @@ program AtomIon1D
      endif
      NumStateInc=NumStates-NumFirst
 
-     xMin = theta_c + asin(dsqrt(mu/(1d0+mu**2))* sNb/R(iR)*(Rstar/lho)) !Based on fixed sNb
-     xMax = Pi + theta_c - asin(dsqrt(mu/(1d0+mu**2))* sNb/R(iR)*(Rstar/lho))
+     xMin = theta_c! + asin(dsqrt(mu/(1d0+mu**2))* sNb/R(iR)*(Rstar/lho)) !Based on fixed sNb
+     xMax = Pi + theta_c! - asin(dsqrt(mu/(1d0+mu**2))* sNb/R(iR)*(Rstar/lho))
 
      sbc = (lho/Rstar)*R(iR)*dsqrt((1d0 + mu**2)/mu)*SIN(xMin - theta_c)
      !print*, 'sbc using sbc = (lho/Rstar)*R(iR)*dsqrt((1+mu**2)/mu)*SIN(xMin - theta_c):', sbc
@@ -268,7 +271,7 @@ program AtomIon1D
      call CalcBasisFuncs(CB%Left,CB%Right,Order,xPoints,LegPoints,xLeg,CB%xDim,CB%xBounds,xNumPoints,0,CB%u)
      call CalcBasisFuncs(CB%Left,CB%Right,Order,xPoints,LegPoints,xLeg,CB%xDim,CB%xBounds,xNumPoints,2,CB%uxx)
      call CalcHSD(alpha,R(iR),mu,mi,theta_c,C4,L,Order,xPoints,&
-          LegPoints,xLeg,wLeg,CB%xDim,xNumPoints,CB%u,CB%uxx,CB%xBounds,HalfBandWidth,CB%H,CB%S,CB%D,CB)
+          LegPoints,xLeg,wLeg,CB%xDim,xNumPoints,CB%u,CB%uxx,CB%xBounds,HalfBandWidth,CB%H,CB%S,CB%D,CB,atest,c0)
      call MyDsband(LSelect,CB%Energies,CB%Psi,MatrixDim,Shift,MatrixDim,CB%H,CB%S,HalfBandWidth+1,LUFac,LeadDim,HalfBandWidth,&
           NumStates,Tol,Residuals,ncv,CB%Psi,MatrixDim,iparam,workd,workl,ncv*ncv+8*ncv,iwork,info)
      call CalcEigenErrors(info,iparam,MatrixDim,CB%H,HalfBandWidth+1,CB%S,HalfBandWidth,NumStates,CB%Psi,CB%Energies,ncv)
@@ -276,53 +279,62 @@ program AtomIon1D
 
      AllPsis(:,:,iR) = Transpose(CB%Psi(:,1:NumStates))
      AllEnergies(:,iR) = CB%Energies(1:NumStates,1)
+!---------------------------------------------------------------------------------------------
+     !Beginning of diabatization
+     
+!!$     call CalcPermutation(NumStates,HalfBandWidth,MatrixDim,OldPsi,NewPsi,CB%S,Perm,ncv,Dtol)
+!!$     if(iR.eq.RSteps) then
+!!$        Perm = 0d0
+!!$        do i = 1,NumStates
+!!$           Perm(i,i) = 1d0
+!!$        enddo
+!!$        ComposedPerm = Perm
+!!$     endif
+!!$
+!!$     ! Construct the composed permutation operator and permute the eigenvectors and eigenvalues
+!!$     ComposedPerm = matmul(ComposedPerm,Perm)
+!!$     AllPsis(:,:,iR) = matmul(ComposedPerm,AllPsis(:,:,iR))
+!!$     AllEnergies(:,iR) = matmul(ComposedPerm,AllEnergies(:,iR))
+!!$
+!!$     NewPsi = 0d0
+!!$     NewPsi(:,1:NumStates) = Transpose(AllPsis(:,:,iR))
+!!$
+!!$     !--- Check the phase consistency of NewPsi, and fix phases ------
+!!$     if(iR.lt.RSteps) then
+!!$        OldPsi = 0d0
+!!$        OldPsi(:,1:NumStates) = Transpose(AllPsis(:,:,iR+1))
+!!$        do j = 1,NumStates
+!!$           call dsbmv('U',MatrixDim,HalfBandWidth,1.0d0,CB%S,HalfBandWidth+1,OldPsi(:,j),1,0.0d0,SPsi,1)  ! Calculate the vector S*OldPsi and store in SPsi            
+!!$           testorth=ddot(MatrixDim,NewPsi(:,j),1,SPsi,1)            
+!!$           if(testorth.lt.0d0) then
+!!$              NewPsi(:,j) = -NewPsi(:,j)
+!!$              AllPsis(j,:,iR) = -AllPsis(j,:,iR)
+!!$           endif
+!!$        enddo
+!!$
+!!$     endif
 
-     call CalcPermutation(NumStates,HalfBandWidth,MatrixDim,OldPsi,NewPsi,CB%S,Perm,ncv,Dtol)
-     if(iR.eq.RSteps) then
-        Perm = 0d0
-        do i = 1,NumStates
-           Perm(i,i) = 1d0
-        enddo
-        ComposedPerm = Perm
-     endif
 
-     ! Construct the composed permutation operator and permute the eigenvectors and eigenvalues
-     ComposedPerm = matmul(ComposedPerm,Perm)
-     AllPsis(:,:,iR) = matmul(ComposedPerm,AllPsis(:,:,iR))
-     AllEnergies(:,iR) = matmul(ComposedPerm,AllEnergies(:,iR))
-
-     NewPsi = 0d0
-     NewPsi(:,1:NumStates) = Transpose(AllPsis(:,:,iR))
-
-     !--- Check the phase consistency of NewPsi, and fix phases ------
-     if(iR.lt.RSteps) then
-        OldPsi = 0d0
-        OldPsi(:,1:NumStates) = Transpose(AllPsis(:,:,iR+1))
-        do j = 1,NumStates
-           call dsbmv('U',MatrixDim,HalfBandWidth,1.0d0,CB%S,HalfBandWidth+1,OldPsi(:,j),1,0.0d0,SPsi,1)  ! Calculate the vector S*OldPsi and store in SPsi            
-           testorth=ddot(MatrixDim,NewPsi(:,j),1,SPsi,1)            
-           if(testorth.lt.0d0) then
-              NewPsi(:,j) = -NewPsi(:,j)
-              AllPsis(j,:,iR) = -AllPsis(j,:,iR)
-           endif
-        enddo
-
-     endif
-     !--------------------------------------------
+     ! End of the diabatization section
+     !------------------------------------------------------------------------------------------------
 
      write(203, 20) R(iR), (CB%Energies(i,1), i = 1,NumStates) ! writing undiabatized energies
 
      !-------- Couplings from the diabatized eigenstates -------------!
-     call CalcCoupling_FH(NumStates,HalfBandWidth,MatrixDim,AllEnergies(:,iR),NewPsi,CB%S,CB%D,P(:,:,iR),QTil(:,:,iR),ncv)      
-
-     write(103,*) R(iR),(P(WriteChannels(NumOutPutChannels-3),WriteChannels(i),iR),i=NumOutputChannels-3,NumOutputChannels)
-     write(104,*) R(iR),(AllEnergies(writechannels(i),iR) - 0.25d0/(2d0*mu*R(iR)**2) &
-          + QTil(WriteChannels(i),WriteChannels(i),iR)/(2d0*mu),i=1,NumOutputChannels)
-     write(106,*) R(iR), (QTil(WriteChannels(i),WriteChannels(i),iR)/(2d0*mu), i=1, NumOutputChannels)
-     !write(104,*) R(iR), (QTil(i,i,iR)*R(iR)**2, i=1,1)
-     !write(104,*) R(iR), ((AllEnergies(i,iR)-(i-1d0+0.5d0))*2*mu*R(iR)**2, i=1,NumStates)
+!!$     call CalcCoupling_FH(NumStates,HalfBandWidth,MatrixDim,AllEnergies(:,iR),NewPsi,CB%S,CB%D,P(:,:,iR),QTil(:,:,iR),ncv)      
+!!$
+!!$     write(103,*) R(iR),(P(WriteChannels(NumOutPutChannels-3),WriteChannels(i),iR),i=NumOutputChannels-3,NumOutputChannels)
+!!$     write(104,*) R(iR),(AllEnergies(writechannels(i),iR) - 0.25d0/(2d0*mu*R(iR)**2) &
+!!$          + QTil(WriteChannels(i),WriteChannels(i),iR)/(2d0*mu),i=1,NumOutputChannels)
+!!$     write(106,*) R(iR), (QTil(WriteChannels(i),WriteChannels(i),iR)/(2d0*mu), i=1, NumOutputChannels)
+!!$     !write(104,*) R(iR), (QTil(i,i,iR)*R(iR)**2, i=1,1)
+!!$     !write(104,*) R(iR), ((AllEnergies(i,iR)-(i-1d0+0.5d0))*2*mu*R(iR)**2, i=1,NumStates)
+!!$
+!!$     OldPsi = CB%Psi
      !--------------------------------------------------------------------------!
-     OldPsi = CB%Psi
+
+
+     
      !    Adjusting Shift
      ur(iR) = CB%Energies(1,1)
      Shift = -200d0
@@ -338,15 +350,15 @@ program AtomIon1D
   do iR = 1, RSteps
      !     write(200,11) R(iR), (AllEnergies(WriteChannels(i),iR), i = 1,NumOutPutChannels) ! Write the diabatized energies
      write(200,11) R(iR), (AllEnergies(i,iR), i = 1,NumStates) ! Write the diabatized energies      
-     write(101,11) R(iR)
-     write(102,11) R(iR)
-     write(105,11) R(iR)
-     do i=1,NumOutputChannels
-        write(101,11) (P(WriteChannels(i),WriteChannels(j),iR), j=1,NumOutputChannels)
-        write(102,11) (QTil(WriteChannels(i),WriteChannels(j),iR), j=1,NumOutputChannels)
-        write(105,11) ((AllEnergies(writechannels(i),iR) - 0.25d0/(2d0*mu*R(iR)**2))*kdelta(i,j) &
-             + QTil(WriteChannels(i),WriteChannels(j),iR)/(2d0*mu), j=1,NumOutputChannels)
-     enddo
+!!$     write(101,11) R(iR)
+!!$     write(102,11) R(iR)
+!!$     write(105,11) R(iR)
+!!$     do i=1,NumOutputChannels
+!!$        write(101,11) (P(WriteChannels(i),WriteChannels(j),iR), j=1,NumOutputChannels)
+!!$        write(102,11) (QTil(WriteChannels(i),WriteChannels(j),iR), j=1,NumOutputChannels)
+!!$        write(105,11) ((AllEnergies(writechannels(i),iR) - 0.25d0/(2d0*mu*R(iR)**2))*kdelta(i,j) &
+!!$             + QTil(WriteChannels(i),WriteChannels(j),iR)/(2d0*mu), j=1,NumOutputChannels)
+!!$     enddo
   enddo
   
   close(11)
@@ -879,7 +891,7 @@ end subroutine CalcOverlap
 ! In the case of CB%x and CB%V the asignment is made directly through CB.
 !
 subroutine CalcHSD(alpha,R,mu,mi,theta_c,C4,L,Order,xPoints,LegPoints,&
-     xLeg,wLeg,xDim,xNumPoints,u,uxx,xBounds,HalfBandWidth,H,S,D,CB)
+     xLeg,wLeg,xDim,xNumPoints,u,uxx,xBounds,HalfBandWidth,H,S,D,CB,atest,c0)
   use BasisSets
   implicit none
   TYPE(basis) CB
@@ -894,7 +906,7 @@ subroutine CalcHSD(alpha,R,mu,mi,theta_c,C4,L,Order,xPoints,LegPoints,&
   integer Row,NewRow,Col
   integer, allocatable :: kxMin(:,:),kxMax(:,:)
   double precision a,b,m,Pi
-  double precision Rall,rai,xai
+  double precision Rall,rai,xai,atest,c0
   double precision u1,sys_ss_pot,V12,V23,V31
   double precision VInt,VTempInt,potvalue, xTempV,xTempS
   double precision x,ax,bx,xScaledZero,xTempT,xTempVHO,xTempVC4
@@ -946,7 +958,7 @@ subroutine CalcHSD(alpha,R,mu,mi,theta_c,C4,L,Order,xPoints,LegPoints,&
         xai = mu**(-0.5d0)*sinx(lx,kx) - (mu**0.5d0)*cosx(lx,kx) 
         XX(lx,kx) = cosx(lx,kx)**2
         YY(lx,kx) = C4/(xai**4)
-        potvalue = -YY(lx,kx)/R**4 + 0.5d0*mu*R*R*XX(lx,kx)
+        potvalue = -YY(lx,kx)/R**4*tanh((R*xai/(2d0*atest))**4) + c0*exp(-(R*xai/atest)**2) + 0.5d0*mu*R*R*XX(lx,kx)
         Pot(lx,kx) = alpha*potvalue
         CB%V(lx,kx) = alpha*potvalue
         !                    write(6,*) 'THIS IS A TEST', kx, lx, Pot(lx,kx)
